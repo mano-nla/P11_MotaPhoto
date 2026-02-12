eba4317ff1c4d91df70e293e4533aba0556b5e5a
Manoelle <manoelle.ancion@gmail.com>
2026-02-12 20:27:36 +0100

    Etape  4 : intégration des 8 premières photos de la liste de photos

--- front-page.php
+++ front-page.php
@@ -2,10 +2,26 @@
 <section class="front-page-hero"> 
 	<h1> Photographe Event </h1>
 </section>
-<section class="front-page-filter"> 
+<main>
+	<section class="front-page-filter"> 
 	
-</section>
-<section class="front-page-photo"> 
-
-</section>
+	</section>
+	<section class="front-page-photo"> 
+		<?php
+    		$args = array(
+        		'post_type'      => 'photos',
+        		'posts_per_page' => 8,
+				'orderby' 		 => 'rand',
+			);
+			$my_query = new WP_Query( $args );
+			if( $my_query->have_posts() ) {
+				while( $my_query->have_posts() ) { 
+					$my_query->the_post();
+        			get_template_part( 'templates_part/photo-block' );
+					}
+			}
+			wp_reset_postdata();
+			?>
+	</section>
+</main>
 <?php get_footer(); ?>
